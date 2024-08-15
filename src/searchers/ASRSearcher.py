91c0af4e1da4c37feba0401ed589841f00f5b6b6
Tuan <tuanstarkpd@gmail.com>
2024-08-15 11:12:43 +0700

    repair ASRSearcher.py

--- src/searchers/ASRSearcher.py
+++ src/searchers/ASRSearcher.py
@@ -5,31 +5,33 @@ from joblib import Parallel, delayed
 import pandas as pd
 
 
-PATH_TO_MAP_KEYFRAMES_FOLDER="./map-keyframes/"
+# PATH_TO_MAP_KEYFRAMES_FOLDER="./map-keyframes/"
 def find_closest_match(query,sentence, threshold=55):
     if query.lower() in sentence["text"].lower():
         return (100, sentence)
-    score = fuzz.ratio(query.lower(), sentence["text"].lower())
-    if score >= threshold:
-            return (score, sentence)
+    # score = fuzz.ratio(query.lower(), sentence["text"].lower())
+    # if score >= threshold:
+    #         return (score, sentence)
     return None
-def get_file_name_img_from_keyframe(vid,lst_frame_idx):
-    df=pd.read_csv(PATH_TO_MAP_KEYFRAMES_FOLDER+vid+".csv")
-    lst=[]
-    for i in range(len(lst_frame_idx)):
-        lst_dis=[]
-        #chỗ này chỉnh lại binary search cho nhanh vì mảng được sắp xếp r.
-        flag=0
-        for j,frame_idx in enumerate(df.frame_idx):
-            if lst_frame_idx[i]-frame_idx<0:
-              flag+=1
-              if flag==2:
-                break
-            distance=abs(frame_idx-lst_frame_idx[i])
-            lst_dis.append(distance)
-        lst.append(df.n[lst_dis.index(min(lst_dis))])
+# def get_file_name_img_from_keyframe(vid,lst_frame_idx):
+#     df=pd.read_csv(PATH_TO_MAP_KEYFRAMES_FOLDER+vid+".csv")
+#     lst=[]
+#     for i in range(len(lst_frame_idx)):
+#         lst_dis=[]
+#         #chỗ này chỉnh lại binary search cho nhanh vì mảng được sắp xếp r.
+#         flag=0
+#         for j,frame_idx in enumerate(df.frame_idx):
+#             if lst_frame_idx[i]-frame_idx<0:
+#               flag+=1
+#               if flag==2:
+#                 break
+#             distance=abs(frame_idx-lst_frame_idx[i])
+#             lst_dis.append(distance)
+#         lst.append(df.n[lst_dis.index(min(lst_dis))])
+#     return lst
 
-    return lst
+
+
 # def get_file_name_img_from_keyframe(vid,lst_frame_idx):
 #     df=pd.read_csv(PATH_TO_MAP_KEYFRAMES_FOLDER+vid+".csv")
 #     lst=[]
@@ -44,8 +46,10 @@ def search_in_db_video(vid, data, query):# tìm trong 1 video
     for idx,sentence in enumerate(data):#duyệt qua từng câu nói trong video
         check = find_closest_match(query, sentence)
         if check is not None:
-            img_names=get_file_name_img_from_keyframe(vid,check[1]['frames'])
-            result.append({"video_name": vid, "keyframe_id": img_names, "score": check[0]})
+            # img_names=get_file_name_img_from_keyframe(vid,check[1]['frames'])
+            # result.append({"video_name": vid, "keyframe_id": img_names, "score": check[0]})
+            result.append({"video_name": vid, "keyframe_id": check[1]['frames'], "score": check[0]})
+
     return result
 
 def ASR_search_engine(query,database,num_img=10):# chạy song song , tìm từng trong từng video. mỗi lần tìm song song trong 14 video
